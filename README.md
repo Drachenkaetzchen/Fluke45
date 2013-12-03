38cbaf6acb6e0d5026c7826d48b3c8d04eaf3404
Timo A. Hummel <felicitus@felicitus.org>
2013-12-03 20:54:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pt1000_temp_sensor.php
+
+Connects to a Fluke 45 multimeter via RS232 and reads out a connected PT1000 temperature sensor
---
+++ pt1000_temp_sensor.php
@@ -0,0 +1,48 @@
+<?php
+include("php_serial.class.php");
+
+$serial = new phpSerial();
+
+
+$serial->deviceSet("/dev/ttyUSB0");
+$serial->confBaudRate(9600);
+$serial->deviceOpen();
+
+$serial->sendMessage("OHMS\n");
+$serial->sendMessage("RATE M\n");
+while (1) {
+	$serial->sendMessage("VAL1?\n");
+	sleep(1);
+	$value = $serial->readPort();
+//	echo $value;
+
+	$parsedValue = floatval($value);
+//	echo $parsedValue."\n";
+	echo number_format(pt1000toC($parsedValue,1000), 2)." °C                 \r";
+
+}
+
+function pt1000toC ($r, $r0) {
+    $a=3.9083E-3; 
+    $b=-5.775E-7; 
+    $t; 
+    
+    $r = $r / $r0; 
+    
+    $t=0.0-$a; 
+    $t+=sqrt(($a*$a) - 4.0 * $b * (1.0 - $r)); 
+    $t/=(2.0 * $b); 
+    
+    if($t>0&&$t<200) { 
+      return $t; 
+    } 
+    else { 
+      //T=  (0.0-A - sqrt((A*A) - 4.0 * B * (1.0 - R))) / 2.0 * B; 
+      $t=0.0-$a; 
+      $t-=sqrt(($a*$a) - 4.0 * $b * (1.0 - $r)); 
+      $t/=(2.0 * $b); 
+      return $t; 
+    } 
+}
+$serial->deviceClose();
+
